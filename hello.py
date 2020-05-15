9b709beafaa17747c89369c4a14a450d6e778dd9
sgrudz <65190790+sgrudz@users.noreply.github.com>
2020-05-15 15:17:09 -0400

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-print("what is your name")
-myname = input() # taking user input
-print("Your name, " + myname)
-print(len(myname)) # getting the length of myname
-
-print("What is your age?")
-myage = input()
-print("In one year, you will be " + str(int(myage) + 1)) #covert input string to integer
-                                              
